96d9487aeccc9a138c3b4793644aefb158a13f70
WEspinoza05 <wjespinoza@correo.url.edu.gt>
2024-02-19 19:52:51 -0600

    feat: bienvenidad commit

---
+++ bienvenida.php
@@ -0,0 +1,14 @@
+<?php
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$numero = $_POST["numero"];
+echo "<p> Bienvenido $nombre  $apellido </p>";
+
+$a = 10;
+$b = $_POST;
+for($i = 1; $i <= $a; $i++)
+{
+    $total = $i * $numero;
+    echo "$numero * ". $i. "=". $total  . "<br>";
+}
+?>
